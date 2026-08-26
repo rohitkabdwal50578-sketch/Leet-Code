8754d2769eaba0300afebb69a17f98148270c2c9
Rohit Kabadwal <rohitkabdwal50578@gmail.com>
2026-08-26 10:07:50 +0530

    Time: 3 ms (45.05%), Space: 9.6 MB (92.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,11 +1,32 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s.compare(t) == 0)
-            return true;
-        return false;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // if(s.compare(t) == 0)
+        //     return true;
+        // return false;
+
+
+
+
+
+        map<char,int> ans;
+        for(auto i : s)
+        {
+            ans[i]++;
+        }
+        for(auto i : t)
+        {
+            ans[i]--;
+        }
+
+        for(auto j : ans)
+        {
+            if(j.second )   // j zero nahi h --> check the value for 0
+            return 0;
+        }
+        return 1;
         
     }
 };
