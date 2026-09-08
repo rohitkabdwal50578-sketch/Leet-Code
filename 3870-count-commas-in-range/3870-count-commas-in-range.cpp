64795ab0b897c4488f820ecdea6d0d72d5c45264
Rohit Kabadwal <rohitkabdwal50578@gmail.com>
2026-09-08 18:30:59 +0530

    Time: 0 ms (100%), Space: 8.5 MB (84.49%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        return max(n-999,0);
+        
+    }
+};
+
+
+//1000 se pehle kisi me comma nahi h.....fir check karo 1000 se n tak kitne number hai (n-1000)+1    
